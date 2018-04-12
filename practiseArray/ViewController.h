cfb3f50dfbf29f7e555f24f4e7a777fe4993ed3a
NataliaMurauyova <natashamur009@gmail.com>
2018-04-12 18:47:00 +0300

    Initial Commit

---
+++ practiseArray/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practiseArray
+//
+//  Created by Наташа on 12.04.18.
+//  Copyright © 2018 Наташа. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
